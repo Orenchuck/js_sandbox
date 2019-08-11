07f0e275ad56ecfb3f491f05f1beeebb145b3418
unknown <golub.ptica@gmail.com>
2019-08-11 19:19:58 +0300

    change middleware

--- lesson_19/middlewares/deduplicator.js
+++ lesson_19/middlewares/deduplicator.js
@@ -1,8 +1,11 @@
 import * as Constants from '../actions';
 
 export default (store) => (next) => (payload) => {
-    if (payload.type === Constants.ADD){
-        next({type: Constants.DEDUPLICATOR});
-    }
+    const { name } = payload;
+    const students = store.getState().students;
+
+    const exist = students.some(el => el.name === name);
+    if (!exist){
         next(payload);
+    }  
 }
